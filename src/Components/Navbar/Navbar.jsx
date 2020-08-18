892a1be052ee55be6567a00d5c72a178e4ce9ac7
Roman Semeniuk <roman.semeniuk@novage.com.ua>
2020-08-18 10:22:54 +0300

    Add NavLink for routes and css style for them

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
     return (
         <div className={styles.nav}>
             <ul>
-                <li><a href="/profile">Profile</a></li>
-                <li><a href="/dialogs">Messages</a></li>
-                <li><a href="/news">News</a></li>
-                <li><a href="/music">Music</a></li>
-                <li><a href="/settings">Settings</a></li>
+                <li className={styles.item}>
+                    <NavLink to="/profile" activeClassName={styles.activeLink}>Profile</NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to="/dialogs" activeClassName={styles.activeLink}>Messages</NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to="/news" activeClassName={styles.activeLink}>News</NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to="/music" activeClassName={styles.activeLink}>Music</NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to="/settings" activeClassName={styles.activeLink}>Settings</NavLink>
+                </li>
             </ul>
         </div>
     )
